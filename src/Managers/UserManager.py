6f02b2930fe44820fb4954459bf60f755a0d177c
Krapp <krapp@sympalog.de>
2025-05-26 22:20:17 +0200

    First Commit, raw UserManager structure

---
+++ src/Managers/UserManager.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+
+
+class ToolContext(BaseModel):
+    type: str                   # none, information, confirmation 
+    requester: str
+    request: str
+
+
+class UserManager:
+
+    def __init__(self):
+        self.context = ToolContext(type="none", requester="", request="")
+        self.prompt: str = ""
+        self.tools: list = [] # handle_IoT, handle_automation, handle_preference, handle_reverse 
+        self.messages: list[dict[str,str]] = []
+
+
+    def handle_user_input(self, query: str):
+
+
+        if self.context.type == "confirmation":
+
+            print("Yes confirmation context response")
+
+            # prompt UM-LLM to classify for confirmation
+
+        elif self.context.type == "information":
+
+            print("Yes information context response")                   
+
+            # prompt UM-LLM to classify for information
+
+        else:
+
+            print("No context response")
+            # get UM-LLM response
+
+            # if LLM calls tools
+
+                # if handle_IoT
+
+                    # get AM.IoT_call response 
+                    # if successfull API call
+                        # return "tell the user success"
+
+                    # else if context request
+                        # set context to confirmation
+                        # return "ask the user for confirmation of..." 
+                        
+                    # else if information request
+                        # set context to information
+                        # return "ask the user for information for ... with arguments ..."
+        
